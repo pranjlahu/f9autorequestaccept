94cf0eb4618f8f2a7f09e605f2358bf092bb1136
pranjlahu <126371854+pranjlahu@users.noreply.github.com>
2023-03-15 15:28:50 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
     FSUB = getenv("FSUB", "F9Devs")
     CHID = int(getenv("CHID", "-1001536495904"))
-    SUDO = list(map(int, getenv("SUDO").split()))
+    SUDO = list(map(int, getenv("SUDO", "6029941553").split()))
     MONGO_URI = getenv("MONGO_URI", "")
     
 cfg = Config()
