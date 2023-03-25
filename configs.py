66fc7d9a61b0db89fafc6914f6bff70cdb707849
pranjlahu <126371854+pranjlahu@users.noreply.github.com>
2023-03-25 22:59:54 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "5989401655:AAFXjsC51YGClf_xv-fHMc4fZlu2i7JJaus")
     FSUB = getenv("FSUB", "F9Devs")
     CHID = int(getenv("CHID", "-1001536495904"))
-    SUDO = list(map(int, getenv("SUDO", "6029941553,1217302537").split()))
+    SUDO = list(map(int, getenv("SUDO", '6029941553,1217302537').split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb://samar:samar@ac-sjxwrus-shard-00-00.ayer8ux.mongodb.net:27017,ac-sjxwrus-shard-00-01.ayer8ux.mongodb.net:27017,ac-sjxwrus-shard-00-02.ayer8ux.mongodb.net:27017/?ssl=true&replicaSet=atlas-ghv0w6-shard-0&authSource=admin&retryWrites=true&w=majority")
     
 cfg = Config()
