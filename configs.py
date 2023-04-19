1ba754c90c7ab9dc400e1002968e1d74a9951def
pranjlahu <126371854+pranjlahu@users.noreply.github.com>
2023-04-20 01:16:17 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = getenv("API_HASH", "027b7feb0a8f9822e53af59706b8f635")
     BOT_TOKEN = getenv("BOT_TOKEN", "5989401655:AAFXjsC51YGClf_xv-fHMc4fZlu2i7JJaus")
     FSUB = getenv("FSUB", "pranjal_movies")
-    CHID = int(getenv("CHID", "-1001536495904"))
+    CHID = int(getenv("CHID", "-1001805259061"))
     SUDO = list(map(int, getenv("SUDO", "6029941553,1217302537").split(",")))
     MONGO_URI = getenv("MONGO_URI", "mongodb://samar:samar@ac-sjxwrus-shard-00-00.ayer8ux.mongodb.net:27017,ac-sjxwrus-shard-00-01.ayer8ux.mongodb.net:27017,ac-sjxwrus-shard-00-02.ayer8ux.mongodb.net:27017/?ssl=true&replicaSet=atlas-ghv0w6-shard-0&authSource=admin&retryWrites=true&w=majority")
     
